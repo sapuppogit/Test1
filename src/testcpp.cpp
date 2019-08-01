58e9f881b4923c2007fee67539faa5bfc7f550b7
sapuppo <sapuppo.git@gmail.com>
2019-08-01 17:17:23 +0200

    testcpp modified

--- src/testcpp.cpp
+++ src/testcpp.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include "calcolatrice.h"
 #include "persona.h"
+#include "animale.h"
 using namespace std;
 
 int main() {
@@ -18,9 +19,16 @@ int main() {
 	cout << dividi(2.5,4.2) << endl;
 
 	persona persona[2];
-	persona[0] = creaPersona("Marco", 24);
-	persona[1] = creaPersona("Samuel", 25);
+	persona[0] = creaAnimale("Marco", 24);
+	persona[1] = creaAnimale("Samuel", 25);
 	cout << persona[0].nome << " " << persona[0].eta << endl;
 	cout << persona[1].nome << " " << persona[1].eta << endl;
+
+	animale animale[2];
+	animale[0] = creaAnimale("Rughetta", 1);
+	animale[1] = creaAnimale("Zampetta", 7);
+	cout << animale[0].nome << " " << animale[0].eta << endl;
+	cout << animale[1].nome << " " << animale[1].eta << endl;
+
 	return 0;
 }
